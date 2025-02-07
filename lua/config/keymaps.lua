0d88981714fd5b73db7a52dc1c81135f40df1dd1
Jake <jake@skibidi.moe>
2025-02-08 00:50:47 +0200

    make more comfy
    add rust LSP, change blink config to supertab, move lualine back to the bottom

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-
 vim.keymap.set("n", "<C-w>", "<cmd>bnext<CR>")
 vim.keymap.set("n", "<C-q>", "<cmd>bprev<CR>")
+vim.keymap.set("i", "jk", "<Esc>", { noremap = false })
--- lua/plugins/lualine-config.lua
+++ lua/plugins/lualine-config.lua
@@ -1,21 +1,22 @@
-return {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-        opts.options = vim.tbl_extend("force", opts.options or {}, {
-            globalstatus = false, -- Disable global statusline
-        })
-
-        -- Use winbar to simulate moving the statusline to the top
-        opts.winbar = opts.sections
-
-        -- Optional: Remove bottom statusline
-        opts.sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {},
-            lualine_x = {},
-            lualine_y = {},
-            lualine_z = {},
-        }
-    end,
-}
+return {}
+-- return {
+--     "nvim-lualine/lualine.nvim",
+--     opts = function(_, opts)
+--         opts.options = vim.tbl_extend("force", opts.options or {}, {
+--             globalstatus = false, -- Disable global statusline
+--         })
+--
+--         -- Use winbar to simulate moving the statusline to the top
+--         opts.winbar = opts.sections
+--
+--         -- Optional: Remove bottom statusline
+--         opts.sections = {
+--             lualine_a = {},
+--             lualine_b = {},
+--             lualine_c = {},
+--             lualine_x = {},
+--             lualine_y = {},
+--             lualine_z = {},
+--         }
+--     end,
+-- }
